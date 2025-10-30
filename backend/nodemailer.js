39a9bddc3652f74d0fc4376672334fbadb2fcbe4
Tarun-Upadhyay-616 <145785799+Tarun-Upadhyay-616@users.noreply.github.com>
2025-10-30 12:07:57 +0530

    Change SMTP port from 587 to 2525

--- backend/nodemailer.js
+++ backend/nodemailer.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const transport = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     auth:{
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
